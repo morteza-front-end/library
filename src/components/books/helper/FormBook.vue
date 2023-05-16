eef68bf09ec403793a052c276f0000404ce177cc
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2023-05-16 19:33:40 +0330

    refactor and handle upload file cover image

--- src/components/books/helper/FormBook.vue
+++ src/components/books/helper/FormBook.vue
@@ -1,19 +1,30 @@
 <template>
-  <v-form @submit.prevent class="border-0">
-    <v-text-field
-      v-for="(key,index) in Object.keys(bookData)"
-      :key="index"
-      v-model="bookData[key]"
-      rules="required"
-      hideDetails
-      :label="key"
-    ></v-text-field>
+  <v-form @submit.prevent="onSubmit" class="border-0">
+    <template v-for="(field,index) in fields"
+              :key="index">
+      <v-file-input
+        v-if="field === 'coverImage'"
+        label="cover image"
+        accept="image/png, image/jpeg, image/bmp"
+        @change="uploadFile"
+        show-size
+      ></v-file-input>
+      <v-text-field
+        v-else
+        :name="field"
+        v-model="bookData[field]"
+        :rules="fieldRules"
+        hideDetails
+        :label="field"
+      ></v-text-field>
+    </template>
+
     <v-btn type="submit" block class="mt-2">Submit</v-btn>
   </v-form>
 </template>
 <script setup>
-
-import {reactive} from "vue";
+import {computed, reactive} from "vue";
+import {useBookStore} from "@/store/books"
 
 const props = defineProps({
   modelValue: {
@@ -22,11 +33,56 @@ const props = defineProps({
     default: false
   }
 })
+const emit = defineEmits(["update:modelValue",])
 let bookData = reactive({
   id: 0,
   name: "",
   author: "",
   coverImage: "",
+  genre: '',
+  pageNumber: "",
   isbn: ""
 })
+let fieldRules = [
+  value => {
+    if (value) return true
+
+    return 'This field is required.'
+  },
+]
+let fields = computed(() => Object.keys(bookData).filter((key) => key !== 'id'))
+
+const {addBooks} = useBookStore()
+
+// upload file
+// conver to base 64
+const toBase64 = file => new Promise((resolve, reject) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = () => resolve(reader.result);
+  reader.onerror = reject;
+});
+
+async function uploadFile(event) {
+  const file = event.target.files[0];
+  bookData.coverImage = await toBase64(file)
+}
+
+function generateRandomId() {
+  return Math.floor(Math.random() * 1000)
+}
+
+async function onSubmit(payload) {
+  let response = await payload
+  if (response.valid) {
+    bookData.id = generateRandomId()
+    let isAdded = await addBooks(bookData)
+    if (isAdded) {
+      emit("update:modelValue", false)
+    } else {
+      emit("update:modelValue", true)
+    }
+  }
+
+}
 </script>
