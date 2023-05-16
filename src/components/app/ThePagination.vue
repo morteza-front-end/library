3b3622f3c4d83c9c77d779e4ef3890f8b08a35a9
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2023-05-16 03:52:55 +0330

    fix warning about import define props and emits

--- src/components/app/ThePagination.vue
+++ src/components/app/ThePagination.vue
@@ -2,22 +2,25 @@
   <v-pagination :length="props.length" v-model="currentPage"></v-pagination>
 </template>
 <script setup>
-import {ref, watch, defineEmits, defineProps} from "vue";
+import { ref, watch } from "vue";
 
 const props = defineProps({
   length: {
     type: [String, Number],
     required: true,
-    default: 1
+    default: 1,
   },
-})
+});
 
-const emit = defineEmits(["change"])
+const emit = defineEmits(["change"]);
 
-const currentPage = ref(1)
-watch(() => currentPage.value, (newValue, oldValue) => {
-  if (newValue !== oldValue) {
-    emit("change", newValue)
+const currentPage = ref(1);
+watch(
+  () => currentPage.value,
+  (newValue, oldValue) => {
+    if (newValue !== oldValue) {
+      emit("change", newValue);
+    }
   }
-})
+);
 </script>
