f628eeaf55e5a16e1537c4a0bce94116cc02c3d5
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2023-05-16 19:35:52 +0330

    use icon

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,12 +7,19 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
-
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 // Composables
 import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
   theme: {
     themes: {
       light: {
